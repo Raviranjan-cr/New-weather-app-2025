c2d4cb1d6dac8fc00798663f6b33ede481114b16
Raviranjan-cr <raviiichy@gmail.com>
2025-01-15 17:53:26 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,70 @@
+import 'package:flutter/material.dart';
+import 'weather_service.dart';
+import 'weather_model.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WeatherScreen(),
+    );
+  }
+}
+
+class WeatherScreen extends StatefulWidget {
+  @override
+  _WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  final WeatherService _weatherService = WeatherService();
+  Future<Weather>? _weather;
+  final TextEditingController _controller = TextEditingController();
+
+  void _searchWeather() {
+    setState(() {
+      _weather = _weatherService.fetchWeather(_controller.text);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter city',
+                border: OutlineInputBorder(),
+              ),
+              onSubmitted: (_) => _searchWeather(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _searchWeather,
+              child: Text('Get Weather'),
+            ),
+            SizedBox(height: 20),
+            if (_weather != null)
+              FutureBuilder<Weather>(
+                future: _weather,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot
